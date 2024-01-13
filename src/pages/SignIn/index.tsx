c90fa2d3b21abda3ad0986177a82ffbdf4627daa
Wagner Terry <goncalveswagner15@gmail.com>
2024-01-12 21:02:44 -0300

    feat: criando componente button

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Content, Title } from './styles';
 import { Input } from '../../components/Form/Input';
 import { ScrollView } from 'react-native';
+import { Button } from '../../components/Form/Button';
 
 export const SignIn: React.FunctionComponent = () => {
   return (
@@ -14,6 +15,8 @@ export const SignIn: React.FunctionComponent = () => {
           <Title>Faça seu login</Title>
           <Input placeholder="Email" />
           <Input placeholder="Senha" />
+
+          <Button title="Entrar" />
         </Content>
       </Container>
     </ScrollView>
