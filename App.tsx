8a88b4852c784fa64f090391ffe36be43d8cb16d
Wagner Terry <goncalveswagner15@gmail.com>
2024-01-25 23:12:58 -0300

    chore: ajuste no Status Bar

--- App.tsx
+++ App.tsx
@@ -4,10 +4,12 @@ import theme from './src/global/styles/theme';
 import { NavigationContainer } from '@react-navigation/native';
 import { Routes } from './src/routes';
 import { AuthProvider } from './src/context/AuthContext';
+import { StatusBar } from 'react-native';
 
 const App: React.FunctionComponent = () => {
   return (
     <NavigationContainer>
+      <StatusBar backgroundColor={'transparent'} translucent />
       <ThemeProvider theme={theme}>
         <AuthProvider>
           <Routes />
