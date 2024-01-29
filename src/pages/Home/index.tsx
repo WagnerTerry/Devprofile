d5b37d246819d8a50f3945738c818ff5076f03f7
Wagner Terry <goncalveswagner15@gmail.com>
2024-01-29 16:12:00 -0300

    feat: criando pagina userDetails

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+
 import {
   Container,
   Header,
@@ -22,9 +24,14 @@ import { IUser } from '../../model/user';
 import { api } from '../../services/api';
 import { User } from '../../components/User';
 
+interface ScreenNavigationProp {
+  navigate: (screen: string, params?: unknown) => void;
+}
+
 export const Home: React.FunctionComponent = () => {
   const [users, setUsers] = React.useState<IUser[]>([]);
   const { user, signOut } = useAuth();
+  const { navigate } = useNavigation<ScreenNavigationProp>();
 
   React.useEffect(() => {
     const loadUsers = async () => {
@@ -34,7 +41,9 @@ export const Home: React.FunctionComponent = () => {
     loadUsers();
   }, []);
 
-  console.log(users);
+  const handleUserDetails = (userId: string) => {
+    navigate('UserDetails', { userId });
+  };
 
   const handleSignOut = () => {
     Alert.alert('Tem certeza?', 'Deseja realmente sair da aplicação?', [
@@ -75,7 +84,9 @@ export const Home: React.FunctionComponent = () => {
       <UserList
         // data={users}
         keyExtractor={item => item.id}
-        renderItem={({ item }) => <User data={item} onPress={() => {}} />}
+        renderItem={({ item }) => (
+          <User data={item} onPress={() => handleUserDetails(item.id)} />
+        )}
         ListHeaderComponent={<UserListHeader>Usuários</UserListHeader>}
         ListEmptyComponent={
           <UserListEmpty>Ops! Ainda não há registros.</UserListEmpty>
